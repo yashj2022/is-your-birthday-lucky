50c925b852c8dcd4d909c9ff45bd94f77bc145a6
yashj2022 <111694454+yashj2022@users.noreply.github.com>
2022-09-24 23:44:07 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const DateOfBirth = document.querySelector("#date-Of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number-button");
+const outputBox = document.querySelector("#output-box");
+
+function compareValues(sum, luckyNumber) {
+  if (sum % luckyNumber === 0) {
+    outputBox.innerText = "Your are Lucky Champ 😍";
+ 
+  } else {
+    outputBox.innerText = "You are not lucky enough🥲";
+    
+  }
+}
+console.log(DateOfBirth.value);
+
+function checkBirthDateIsLucky() {
+  const dob = DateOfBirth.value;
+  const sum = calculateSum(dob);
+  if (sum && dob) compareValues(sum, luckyNumber.value);
+  else {
+    outputBox.innerText = "Enter both fields";
+  }
+}
+
+function calculateSum(dob) {
+  dob = dob.replaceAll("-", "");
+  let sum = 0;
+  for (let index = 0; index < dob.length; index++) {
+    sum = sum + Number(dob.charAt(index));
+  }
+  return sum;
+}
+checkNumberButton.addEventListener("click", checkBirthDateIsLucky);
